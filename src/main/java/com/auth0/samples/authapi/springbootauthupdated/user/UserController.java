b70609d009a0fb5538500155176e6134b52d34e2
Paul Kelly <pkelw@allstate.com>
2019-02-26 11:07:27 +0000

    finsihed jwt

---
+++ src/main/java/com/auth0/samples/authapi/springbootauthupdated/user/UserController.java
@@ -0,0 +1,34 @@
+package com.auth0.samples.authapi.springbootauthupdated.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private ApplicationUserRepository applicationUserRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(ApplicationUserRepository applicationUserRepository,
+                          BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.applicationUserRepository = applicationUserRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @PostMapping("/sign-up")
+    public void signUp(@RequestBody ApplicationUser user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        applicationUserRepository.save(user);
+    }
+
+    @RequestMapping(value = {"/user"}, method = RequestMethod.GET)
+    public ResponseEntity<Principal> user(Principal principal, HttpServletRequest request) {
+
+        return ResponseEntity.ok(principal);
+    }
+}
